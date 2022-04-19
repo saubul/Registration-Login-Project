b9f3e7ba3be3cff1df7c1d25f384ea9c35685261
Ruslan <miraxle@yandex.ru>
2022-04-19 16:20:09 +0300

    Инициализация проекта

---
+++ src/main/java/ru/saubulprojects/reglogproject/RegistrationLoginSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.saubulprojects.reglogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/saubulprojects/reglogproject/RegistrationLoginSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saubulprojects.reglogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
